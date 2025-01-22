6c3b03508e49ddda33b2cd93821b455593bcedc5
calebgl <57515964+calebgl@users.noreply.github.com>
2025-01-21 23:14:28 -0600

    refactor: use fluent api instead of annotations

--- Data/BibliotekContext.cs
+++ Data/BibliotekContext.cs
@@ -7,9 +7,34 @@ public class BibliotekContext : DbContext
         : base(options) { }
 
     public DbSet<Book> Books { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Review> Reviews { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Book>().ToTable("books");
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.ToTable("books");
+            entity.Property(b => b.Id).ValueGeneratedOnAdd();
+            entity.Property(b => b.PublishedAt).HasColumnType("date");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("users");
+            entity.Property(u => u.Id).ValueGeneratedOnAdd();
+        });
+
+        modelBuilder.Entity<Review>(entity =>
+        {
+            entity.ToTable("reviews");
+            entity.Property(r => r.Id).ValueGeneratedOnAdd();
+            entity.Property(r => r.Rate).HasPrecision(2, 1).HasColumnType("float").IsRequired();
+
+            entity.HasOne(r => r.User).WithMany(u => u.Reviews).HasForeignKey(r => r.UserId);
+            entity.HasOne(r => r.Book).WithMany(b => b.Reviews).HasForeignKey(r => r.BookId);
+        });
+
+        base.OnModelCreating(modelBuilder);
     }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,17 +1,16 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Bibliotek.Models;
 
 public class Book
 {
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Book()
+    {
+        Reviews = new List<Review>();
+    }
+
     public uint Id { get; set; }
-
     public required string Title { get; set; }
-
     public required string Author { get; set; }
+    public DateOnly PublishedAt { get; set; }
 
-    [DataType(DataType.Date)]
-    public DateOnly? PublishedAt { get; set; }
+    public List<Review> Reviews { get; set; }
 }
