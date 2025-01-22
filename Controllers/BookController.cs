f9ee09966d43866c8b1374da7ad355991d5f4c1c
calebgl <57515964+calebgl@users.noreply.github.com>
2025-01-21 23:14:48 -0600

    fix: list books with latest 5 reviews

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,5 +1,6 @@
-using Bibliotek.Models;
+// using Bibliotek.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bibliotek.Controllers;
 
@@ -8,20 +9,45 @@ namespace Bibliotek.Controllers;
 public class BookController(BibliotekContext context) : ControllerBase
 {
     [HttpGet]
-    public ActionResult<List<Book>> Get()
+    public IActionResult Get()
     {
-        return context.Books.ToList();
+        return Ok(
+            context
+                .Books.Include(b => b.Reviews)
+                .Select(b => new
+                {
+                    b.Id,
+                    b.Title,
+                    Reviews = b
+                        .Reviews.OrderByDescending(r => r.CreatedAt)
+                        .Take(5)
+                        .Select(r => new
+                        {
+                            r.Id,
+                            r.Rate,
+                            r.UserId,
+                            Username = r.User.Username,
+                            Comment = ProcessComment(r.Comment),
+                            r.CreatedAt,
+                            r.UpdatedAt,
+                        }),
+                })
+                .ToList()
+        );
     }
 
-    [HttpGet("{id}")]
-    public ActionResult<Book> Get(uint id)
+    private string? ProcessComment(string? comment)
     {
-        var book = context.Books.Find(id);
-        if (book is null)
+        if (string.IsNullOrWhiteSpace(comment))
         {
-            return NotFound();
+            return null;
         }
 
-        return book;
+        if (comment.Length < 20)
+        {
+            return comment;
+        }
+
+        return comment.Substring(0, 20).TrimEnd() + "...";
     }
 }
