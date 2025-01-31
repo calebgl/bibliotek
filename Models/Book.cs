d9128f3ed8e5878af698e48f86475a31e6cada85
calebgl <57515964+calebgl@users.noreply.github.com>
2025-01-30 23:20:33 -0600

    fix: add missing book properties for store

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,12 +6,16 @@ public class Book
     {
         Reviews = new List<Review>();
         BookStats = new BookStats { Book = this };
+        StockQuantity = 0;
     }
 
     public uint Id { get; set; }
     public required string Title { get; set; }
     public required string Author { get; set; }
     public DateOnly? PublishedAt { get; set; }
+
+    public uint StockQuantity { get; set; }
+    public decimal Price { get; set; }
 
     public List<Review> Reviews { get; set; }
     public BookStats BookStats { get; set; }
