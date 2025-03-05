475041e93ddd401ca977369b811a04692f1ad37b
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-04 18:42:50 -0600

    refactor(ui): extract navbar component

--- Ui/src/views/book.tsx
+++ Ui/src/views/book.tsx
@@ -1,14 +1,12 @@
 import dayjs from 'dayjs'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { fetchBook, fetchReviews } from '../lib/api'
 import { formatCurrency, sleep } from '../lib/utils'
 import type { Book, Review } from '../types'
+import { Navbar } from '../components/navbar'
 
 export function Book() {
-	const [toggleCart, setToggleCart] = useState<boolean>(false)
-	const modalRef = useRef<HTMLDivElement>(null)
-
 	const [book, setBook] = useState<Book | null>(null)
 	const [reviews, setReviews] = useState<Review[]>([])
 
@@ -24,57 +22,9 @@ export function Book() {
 		})()
 	}, [])
 
-	useEffect(() => {
-		function ev(e: KeyboardEvent) {
-			if (toggleCart && e.code === 'Escape') {
-				setToggleCart(false)
-			}
-		}
-
-		window.addEventListener('keyup', ev)
-
-		return () => {
-			window.removeEventListener('keyup', ev)
-		}
-	}, [toggleCart])
-
 	return (
 		<>
-			{toggleCart && (
-				<div
-					ref={modalRef}
-					className="fixed inset-0 z-50 overflow-y-hidden bg-gray-900/30 backdrop-blur-xs"
-					onClick={(e) => {
-						if (e.target === modalRef.current) {
-							setToggleCart(false)
-						}
-					}}
-				>
-					<div className="fixed top-0 right-0 h-full w-1/3 bg-white"></div>
-				</div>
-			)}
-			<div className="sticky top-0 left-0">
-				<div className="grid grid-cols-3 justify-between p-4">
-					<div>logo</div>
-					<div className="justify-self-center">searchbar</div>
-					<div className="flex gap-2 justify-self-end">
-						<button onClick={() => setToggleCart((prev) => !prev)}>
-							cart
-						</button>
-						<button>saved</button>
-						<button>profile</button>
-					</div>
-				</div>
-				<div className="p-4 pt-0">
-					<nav className="flex justify-stretch gap-2">
-						<span>1</span>
-						<span>2</span>
-						<span>3</span>
-						<span>4</span>
-						<span>5</span>
-					</nav>
-				</div>
-			</div>
+			<Navbar />
 			<div>
 				<div className="space-y-16 p-4">
 					<div className="flex gap-8">
