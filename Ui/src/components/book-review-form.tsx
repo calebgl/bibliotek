027679fb3bda67f4b89010335825c07035d40198
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-05 15:50:12 -0600

    feat(ui): add reviews to books

--- Ui/src/components/book-review-form.tsx
+++ Ui/src/components/book-review-form.tsx
@@ -1,6 +1,50 @@
 import dayjs from 'dayjs'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { useParams } from 'react-router'
+
+import { fetchReviews, postReview } from '../lib/api'
+import useSWR from 'swr'
 
 export function BookReviewForm() {
+	const params = useParams()
+	const bookId = params.bookId
+	if (!bookId) {
+		throw new Error('bookId is required on review form')
+	}
+
+	const { data: reviews, mutate } = useSWR('/api/reviews/' + bookId, () =>
+		fetchReviews(bookId),
+	)
+
+	const [rate, setRate] = useState<number>(5)
+	const [comment, setComment] = useState<string>('')
+
+	const user = {
+		id: 1,
+		username: 'calebgl',
+	}
+
+	async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault()
+
+		if (!bookId) {
+			throw new Error('bookId is required on review form')
+		}
+
+		const commentNew = await postReview(
+			user.id,
+			parseInt(bookId),
+			rate,
+			comment,
+		)
+
+		mutate([commentNew, ...(reviews ?? [])])
+	}
+
+	function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+		setComment(event.currentTarget.value)
+	}
+
 	return (
 		<div className="grid grid-cols-[auto_1fr] gap-2">
 			<div className="size-12 bg-violet-200"></div>
@@ -9,11 +53,21 @@ export function BookReviewForm() {
 					<div>calebgl</div>
 					<div>{dayjs().format('LLL')}</div>
 				</div>
-				<textarea
-					placeholder="Leave a review..."
-					className="w-full max-w-prose resize-none bg-white"
-					rows={5}
-				></textarea>
+				<form onSubmit={handleSubmit}>
+					<textarea
+						placeholder="Leave a review..."
+						className="w-full max-w-prose resize-none bg-white"
+						rows={5}
+						value={comment}
+						onChange={handleChange}
+					></textarea>
+					<button
+						type="submit"
+						className="ml-auto cursor-pointer bg-gray-300 px-4 py-2 active:bg-amber-500"
+					>
+						comment
+					</button>
+				</form>
 			</div>
 		</div>
 	)
--- Ui/src/lib/api.ts
+++ Ui/src/lib/api.ts
@@ -1,6 +1,15 @@
 import { Book, Review } from '../types'
 import { sleep } from './utils'
 
+enum Method {
+	GET = 'GET',
+	POST = 'POST',
+}
+
+enum MimeType {
+	JSON = 'application/json',
+}
+
 export async function fetchBook(id: string): Promise<Book> {
 	const resp = await fetch('/api/books/' + id)
 	if (!resp.ok) {
@@ -19,6 +28,31 @@ export async function fetchReviews(bookId: string): Promise<Review[]> {
 	}
 
 	await sleep()
+
+	return resp.json()
+}
+
+export async function postReview(
+	userId: number,
+	bookId: number,
+	rate: number,
+	comment: string,
+): Promise<Review> {
+	const resp = await fetch('/api/reviews', {
+		method: Method.POST,
+		headers: {
+			'Content-Type': MimeType.JSON,
+			'Bibliotek-User-Id': String(userId),
+		},
+		body: JSON.stringify({
+			bookId,
+			rate,
+			comment,
+		}),
+	})
+	if (!resp.ok) {
+		throw new Error('error posting review for book ' + bookId)
+	}
 
 	return resp.json()
 }
