484fa07f92482bb07fb888cd89173ba51da89643
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-08 13:12:45 -0600

    chore: add seed data for cart

--- Ui/src/stores/cart.ts
+++ Ui/src/stores/cart.ts
@@ -7,7 +7,32 @@ type CartBook = Omit<
 	'stars' | 'totalReviews' | 'averageRating' | 'description'
 > & { quantity: number }
 
-export const booksAtom = atom<Record<string, CartBook>>({})
+export const booksAtom = atom<Record<string, CartBook>>({
+	'1': {
+		id: 1,
+		title: '1984',
+		author: 'Goerge Orwell',
+		price: 10.99,
+		coverUrl: 'http://localhost:5173/1984.jpg',
+		quantity: 2,
+	},
+	'2': {
+		id: 2,
+		title: 'Words of Radiance',
+		author: 'Brandon Sanderson',
+		price: 8.99,
+		coverUrl: 'http://localhost:5173/wor.jpg',
+		quantity: 1,
+	},
+	'3': {
+		id: 3,
+		title: 'La Sombra del Viento',
+		author: 'Carlos Zafon',
+		price: 5.99,
+		coverUrl: 'http://localhost:5173/sv.jpg',
+		quantity: 1,
+	},
+})
 
 export const countAtom = atom<number>((get) => {
 	const books = Object.values(get(booksAtom))
