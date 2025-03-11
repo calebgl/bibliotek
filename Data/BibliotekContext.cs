3a6f8b95279a74772f3a0f2aed799a55956e9449
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-11 16:07:19 -0600

    fix: compute timestamp columns when saving them to database

--- Data/BibliotekContext.cs
+++ Data/BibliotekContext.cs
@@ -129,4 +129,36 @@ public class BibliotekContext : DbContext
 
         base.OnModelCreating(modelBuilder);
     }
+
+    public override int SaveChanges()
+    {
+        UpdateTimestamps();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        UpdateTimestamps();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void UpdateTimestamps()
+    {
+        var entries = ChangeTracker
+            .Entries()
+            .Where(e =>
+                e.Entity is ITimestamps
+                && (e.State == EntityState.Added || e.State == EntityState.Modified)
+            );
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property("CreatedAt").CurrentValue = DateTime.UtcNow;
+            }
+
+            entry.Property("UpdatedAt").CurrentValue = DateTime.UtcNow;
+        }
+    }
 }
