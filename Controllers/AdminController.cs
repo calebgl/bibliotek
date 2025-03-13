60e4d39a71adb4735805e75d078b13ebae772d1c
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-13 17:18:59 -0600

    feat(api): endpoints to view and edit a book as admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -34,6 +34,71 @@ public class AdminController(BibliotekContext context) : ControllerBase
         );
     }
 
+    [HttpGet("books/{bookId}")]
+    public IActionResult ViewBook(uint bookId)
+    {
+        var book = context
+            .Books.Where(b => b.Id == bookId)
+            .Select(b => new
+            {
+                b.Id,
+                b.Title,
+                b.Subtitle,
+                b.Author,
+                b.CoverUrl,
+                b.Description,
+                b.Price,
+                b.StockQuantity,
+                b.CreatedAt,
+                b.UpdatedAt,
+            })
+            .SingleOrDefault();
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(book);
+    }
+
+    [HttpPut("books/{bookId}")]
+    public IActionResult EditBook(uint bookId, CreateBookDto book)
+    {
+        var bookToUpdate = context.Books.Where(b => b.Id == bookId).SingleOrDefault();
+        if (bookToUpdate is null)
+        {
+            return NotFound();
+        }
+
+        Console.WriteLine(book);
+
+        bookToUpdate.Title = book.Title;
+        bookToUpdate.Subtitle = book.Subtitle;
+        bookToUpdate.Author = book.Author;
+        bookToUpdate.Description = book.Description;
+        bookToUpdate.Price = book.Price;
+        bookToUpdate.StockQuantity = book.StockQuantity;
+
+        context.Books.Update(bookToUpdate);
+        context.SaveChanges();
+
+        return Ok(
+            new
+            {
+                bookToUpdate.Id,
+                bookToUpdate.Title,
+                bookToUpdate.Author,
+                bookToUpdate.Subtitle,
+                bookToUpdate.CoverUrl,
+                bookToUpdate.Description,
+                bookToUpdate.Price,
+                bookToUpdate.StockQuantity,
+                bookToUpdate.CreatedAt,
+                bookToUpdate.UpdatedAt,
+            }
+        );
+    }
+
     [HttpPost("books")]
     public IActionResult Create(CreateBookDto book)
     {
