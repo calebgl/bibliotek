454af2c2cf583e100ac8e71e1cd04abff6884cee
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-13 21:51:22 -0600

    fix(ui): wrap page inside container

--- Ui/src/views/book.tsx
+++ Ui/src/views/book.tsx
@@ -7,25 +7,27 @@ import { BookReviewList } from '../components/book-review-list'
 
 export function Book() {
 	return (
-		<div className="space-y-16 p-4">
-			<div className="flex gap-8">
-				<div className="basis-1/2">
-					<BookImagePreview />
+		<div className="container mx-auto">
+			<div className="space-y-16 p-4">
+				<div className="flex gap-8">
+					<div className="basis-1/2">
+						<BookImagePreview />
+					</div>
+					<div className="flex basis-1/2 flex-col">
+						<BookPurchaseInfo />
+					</div>
 				</div>
-				<div className="flex basis-1/2 flex-col">
-					<BookPurchaseInfo />
+				<div>
+					<BookDetails />
 				</div>
-			</div>
-			<div>
-				<BookDetails />
-			</div>
-			<div className="flex gap-16">
-				<div className="basis-2/3 space-y-16">
-					<BookReviewForm />
-					<BookReviewList />
-				</div>
-				<div className="basis-1/3">
-					<BookRating />
+				<div className="flex gap-16">
+					<div className="basis-2/3 space-y-16">
+						<BookReviewForm />
+						<BookReviewList />
+					</div>
+					<div className="basis-1/3">
+						<BookRating />
+					</div>
 				</div>
 			</div>
 		</div>
