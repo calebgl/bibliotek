25c086c99987f5158eecd1082ee94703929d38da
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-20 15:37:41 -0600

    feat(ui): add skeleton for reviews

--- Ui/src/components/book-review.tsx
+++ Ui/src/components/book-review.tsx
@@ -10,7 +10,7 @@ type BookReviewProps = HTMLAttributes<HTMLDivElement> & {
 export function BookReview(props: BookReviewProps) {
 	return (
 		<div className={props.className + ' grid grid-cols-[auto_1fr] gap-2'}>
-			<div className="size-12 bg-violet-200"></div>
+			<div className="size-12 bg-violet-200" />
 			<div className="max-w-prose">
 				<div className="flex justify-between">
 					<div>{props.username}</div>
@@ -33,14 +33,22 @@ export function BookReview(props: BookReviewProps) {
 BookReview.Skeleton = () => {
 	return (
 		<div className="grid grid-cols-[auto_1fr] gap-2">
-			<div className="size-12 bg-violet-200" />
-			<div className="max-w-prose">
+			<div className="size-12 animate-pulse bg-violet-200" />
+			<div className="max-w-prose space-y-1">
 				<div className="flex justify-between">
-					<div />
-					<div />
+					<div className="h-3 w-32 animate-pulse bg-gray-200" />
+					<div className="h-3 w-16 animate-pulse bg-gray-100" />
 				</div>
-				<div className="max-w-prose">
-					<div />
+				<div className="flex max-w-prose flex-wrap gap-1">
+					{Array.from({ length: 5 }, (_, index) => (
+						<div
+							key={'review-comment-' + index}
+							className="h-3 animate-pulse bg-gray-100"
+							style={{
+								width: Math.max(0.2, Math.random()) * 100 + '%',
+							}}
+						/>
+					))}
 				</div>
 				<div className="mt-12 flex gap-2">
 					<button className="size-8 cursor-pointer bg-gray-300 px-2 py-1">
