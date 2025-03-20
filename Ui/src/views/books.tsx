866e3f83f83fac21fe91214493778584e914e08c
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-20 15:38:22 -0600

    refactor(ui): separate book list into its component to avoid styles duplication

--- Ui/src/views/books.tsx
+++ Ui/src/views/books.tsx
@@ -5,118 +5,75 @@ import { assert } from '../lib/assert'
 import { formatCurrency } from '../lib/utils'
 
 export function Books() {
-	const { data: books, isLoading, error } = useBooks()
-	if (isLoading) {
-		return (
-			<div className="container mx-auto">
-				<div className="grid grid-cols-4 gap-8">
-					{Array.from({ length: 8 }, (_, index) => {
-						return (
-							<div key={'skeleton-book-' + index} className="">
-								<div className="aspect-[8/9] bg-white p-8">
-									<div className="mx-auto aspect-[2/3] max-h-64 animate-pulse bg-gray-200" />
-								</div>
-								<div className="space-y-1 px-4 py-4">
-									<div className="line-clamp-2">
-										<div
-											className="h-4 animate-pulse bg-gray-300"
-											style={{
-												width:
-													Math.max(
-														0.5,
-														Math.random(),
-													) *
-														100 +
-													'%',
-											}}
-										/>
-									</div>
-									<div className="text-sm text-neutral-500">
-										<div
-											className="h-3 animate-pulse bg-gray-200"
-											style={{
-												width:
-													Math.max(
-														0.5,
-														Math.random(),
-													) *
-														100 +
-													'%',
-											}}
-										/>
-									</div>
-									<div className="flex items-center gap-1">
-										<span>★★★★☆</span>
-										<span className="text-sm">
-											<div />
-										</span>
-										<span className="text-xs">
-											<div />
-										</span>
-									</div>
-									<div>
-										<div className="h-4 w-8 animate-pulse bg-gray-200" />
-									</div>
-								</div>
-							</div>
-						)
-					})}
-				</div>
+	return (
+		<div className="container mx-auto">
+			<div className="grid grid-cols-4 gap-8">
+				<List />
 			</div>
-		)
-	}
+		</div>
+	)
+}
+
+function List() {
+	const { data: books, isLoading, error } = useBooks()
 	if (error) {
 		throw error
+	}
+	if (isLoading) {
+		return Array.from({ length: 8 }, (_, index) => (
+			<div key={'skeleton-book-' + index} className="">
+				<div className="aspect-[8/9] bg-white p-8">
+					<div className="mx-auto aspect-[2/3] max-h-64 animate-pulse bg-gray-200" />
+				</div>
+				<div className="space-y-1 px-4 py-4">
+					<div
+						className="h-4 animate-pulse bg-gray-300"
+						style={{
+							width: Math.max(0.5, Math.random()) * 100 + '%',
+						}}
+					/>
+					<div
+						className="h-3 animate-pulse bg-gray-200"
+						style={{
+							width: Math.max(0.5, Math.random()) * 100 + '%',
+						}}
+					/>
+					<div />
+					<div className="h-4 w-8 animate-pulse bg-gray-200" />
+				</div>
+			</div>
+		))
 	}
 
 	assert(books)
 
-	return (
-		<div className="container mx-auto">
-			<div className="grid grid-cols-4 gap-8">
-				{books.map((book) => {
-					return (
-						<div key={book.id} className="">
-							<div className="aspect-[8/9] bg-white p-8">
-								<Link
-									to={'/books/' + book.id}
-									className="h-full"
-								>
-									<img
-										src={book.coverUrl ?? undefined}
-										alt={book.title}
-										className="mx-auto block h-full max-h-64"
-									/>
-								</Link>
-							</div>
-							<div className="px-4 py-4">
-								<div className="line-clamp-2">
-									<Link to={'/books/' + book.id}>
-										{book.title}
-									</Link>
-								</div>
-								<div className="text-sm text-neutral-500">
-									of {book.author}
-								</div>
-								<div className="flex items-center gap-1">
-									<span>★★★★☆</span>
-									<span className="text-sm">
-										{book.averageRating}
-									</span>
-									<span className="text-xs">
-										({book.totalReviews})
-									</span>
-								</div>
-								<div>
-									<Link to={'/books/' + book.id}>
-										{formatCurrency(book.price)}
-									</Link>
-								</div>
-							</div>
-						</div>
-					)
-				})}
+	return books.map((book) => (
+		<div key={book.id} className="">
+			<div className="aspect-[8/9] bg-white p-8">
+				<Link to={'/books/' + book.id} className="h-full">
+					<img
+						src={book.coverUrl ?? undefined}
+						alt={book.title}
+						className="mx-auto block h-full max-h-64"
+					/>
+				</Link>
+			</div>
+			<div className="px-4 py-4">
+				<div className="line-clamp-2">
+					<Link to={'/books/' + book.id}>{book.title}</Link>
+				</div>
+				<div className="text-sm text-neutral-500">of {book.author}</div>
+				<div className="flex items-center gap-1">
+					<span>★★★★☆</span>
+					<span className="text-sm">{book.averageRating}</span>
+					<span className="text-xs">({book.totalReviews})</span>
+				</div>
+				<div>
+					<Link to={'/books/' + book.id}>
+						{formatCurrency(book.price)}
+					</Link>
+				</div>
 			</div>
 		</div>
-	)
+	))
 }
