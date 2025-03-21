af66c377a7d7d023a0f449398a5c0d253db1a979
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-21 17:19:26 -0600

    refactor: add db migrations

---
+++ Migrations/20250321215643_init.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bibliotek.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    Subtitle = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    CoverUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    ReleasedAt = table.Column<DateOnly>(type: "date", nullable: true),
+                    StockQuantity = table.Column<uint>(type: "INTEGER", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_statuses",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StatusName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order_statuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "books_stats",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BookId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    OneStarReviewCount = table.Column<uint>(type: "INTEGER", nullable: false),
+                    TwoStarReviewCount = table.Column<uint>(type: "INTEGER", nullable: false),
+                    ThreeStarReviewCount = table.Column<uint>(type: "INTEGER", nullable: false),
+                    FourStarReviewCount = table.Column<uint>(type: "INTEGER", nullable: false),
+                    FiveStarReviewCount = table.Column<uint>(type: "INTEGER", nullable: false),
+                    TotalReviewCount = table.Column<uint>(type: "INTEGER", nullable: false),
+                    AverageRating = table.Column<decimal>(type: "decimal(2, 1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books_stats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_books_stats_books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    OrderStatusId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_orders_order_statuses_OrderStatusId",
+                        column: x => x.OrderStatusId,
+                        principalTable: "order_statuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_orders_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "payment_methods",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    PaymentType = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsDefault = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_payment_methods", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_payment_methods_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reviews",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Rate = table.Column<decimal>(type: "decimal(2, 1)", precision: 2, scale: 1, nullable: false),
+                    Comment = table.Column<string>(type: "TEXT", nullable: true),
+                    UserId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    BookId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_reviews_books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_reviews_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_details",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OrderId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    BookId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<uint>(type: "INTEGER", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(5, 2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order_details", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_order_details_books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_order_details_orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "payments_card",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PaymentMethodId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Number = table.Column<string>(type: "TEXT", nullable: false),
+                    CVV = table.Column<string>(type: "TEXT", nullable: false),
+                    ExpiresAt = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_payments_card", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_payments_card_payment_methods_PaymentMethodId",
+                        column: x => x.PaymentMethodId,
+                        principalTable: "payment_methods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "payments_cash",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PaymentMethodId = table.Column<uint>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_payments_cash", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_payments_cash_payment_methods_PaymentMethodId",
+                        column: x => x.PaymentMethodId,
+                        principalTable: "payment_methods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "payments_paypal",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PaymentMethodId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_payments_paypal", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_payments_paypal_payment_methods_PaymentMethodId",
+                        column: x => x.PaymentMethodId,
+                        principalTable: "payment_methods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_books_stats_BookId",
+                table: "books_stats",
+                column: "BookId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_details_BookId",
+                table: "order_details",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_details_OrderId",
+                table: "order_details",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_OrderStatusId",
+                table: "orders",
+                column: "OrderStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_UserId",
+                table: "orders",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payment_methods_UserId",
+                table: "payment_methods",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payments_card_PaymentMethodId",
+                table: "payments_card",
+                column: "PaymentMethodId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payments_cash_PaymentMethodId",
+                table: "payments_cash",
+                column: "PaymentMethodId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payments_paypal_PaymentMethodId",
+                table: "payments_paypal",
+                column: "PaymentMethodId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reviews_BookId",
+                table: "reviews",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reviews_UserId",
+                table: "reviews",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books_stats");
+
+            migrationBuilder.DropTable(
+                name: "order_details");
+
+            migrationBuilder.DropTable(
+                name: "payments_card");
+
+            migrationBuilder.DropTable(
+                name: "payments_cash");
+
+            migrationBuilder.DropTable(
+                name: "payments_paypal");
+
+            migrationBuilder.DropTable(
+                name: "reviews");
+
+            migrationBuilder.DropTable(
+                name: "orders");
+
+            migrationBuilder.DropTable(
+                name: "payment_methods");
+
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "order_statuses");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<BibliotekContext>();
     context.Database.OpenConnection();
-    context.Database.EnsureCreated();
+    context.Database.Migrate();
 
     DbInitializer.Initialize(context);
 }
