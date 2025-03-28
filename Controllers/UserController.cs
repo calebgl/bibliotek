b50c8054b0608941cc203749a30ad5be5bc9d306
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-28 16:06:27 -0600

    chore(api): remove unused endpoints

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -9,23 +9,6 @@ namespace Bibliotek.Controllers;
 [Route("api/users")]
 public class UserController(BibliotekContext context) : ControllerBase
 {
-    [HttpPost]
-    public IActionResult Register(RegisterUserDto user)
-    {
-        var newUser = new User { UserName = user.UserName };
-
-        context.Users.Add(newUser);
-        context.SaveChanges();
-
-        return Ok(new { newUser.Id, newUser.UserName });
-    }
-
-    [HttpGet]
-    public IActionResult ListUsers()
-    {
-        return Ok(context.Users.Select(u => new { u.Id, u.UserName }).ToList());
-    }
-
     [HttpGet("favorites")]
     [Authorize]
     public IActionResult ListFavoriteBooks()
