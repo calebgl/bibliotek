e4657ca8d97eefbd2ebfd6fbaf591a624b18473e
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-28 12:37:12 -0600

    feat(ui): show/hide button based on user session

--- Ui/src/components/navbar-actions.tsx
+++ Ui/src/components/navbar-actions.tsx
@@ -1,9 +1,12 @@
 import { useAtomValue } from 'jotai'
+import { Link } from 'react-router'
 
+import { useAuth } from '../hooks/use-auth'
 import { assert } from '../lib/assert'
 import { countAtom } from '../stores/cart'
 
 export function NavbarActions(props: { onCloseCart(): void }) {
+	const { user, logout } = useAuth()
 	const count = useAtomValue(countAtom)
 	assert(count >= 0)
 
@@ -12,8 +15,14 @@ export function NavbarActions(props: { onCloseCart(): void }) {
 			<button onClick={props.onCloseCart}>
 				cart{count > 0 && <span>({count})</span>}
 			</button>
-			<button>saved</button>
-			<button>profile</button>
+			{user && <button>saved</button>}
+			{user && <button>profile</button>}
+			{user && <button onClick={logout}>logout</button>}
+			{!user && (
+				<button>
+					<Link to={'/login'}>login</Link>
+				</button>
+			)}
 		</>
 	)
 }
