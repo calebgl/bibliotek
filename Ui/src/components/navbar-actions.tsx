aab3235dd4ea3f1dde45bbb39b8a80885cab2efb
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-28 17:09:54 -0600

    feat(ui): prevent cart drawer from opening on /cart view

--- Ui/src/components/navbar-actions.tsx
+++ Ui/src/components/navbar-actions.tsx
@@ -1,28 +1,47 @@
 import { useAtomValue } from 'jotai'
-import { Link } from 'react-router'
+import { ReactNode } from 'react'
+import { Link, useLocation } from 'react-router'
 
 import { useAuth } from '../hooks/use-auth'
 import { assert } from '../lib/assert'
 import { countAtom } from '../stores/cart'
 
 export function NavbarActions(props: { onCloseCart(): void }) {
+	const location = useLocation()
 	const { user, logout } = useAuth()
 	const count = useAtomValue(countAtom)
 	assert(count >= 0)
 
+	const isLocationCart = location.pathname !== '/cart'
+
+	const actions: ReactNode[] = []
+	if (user) {
+		actions.push(
+			<button key="/saved">
+				<Link to={'/saved'}>saved</Link>
+			</button>,
+			<button key="/profile">profile</button>,
+			<button key="/logout" onClick={logout}>
+				logout
+			</button>,
+		)
+	} else {
+		actions.push(
+			<button key="/login">
+				<Link to={'/login'}>login</Link>
+			</button>,
+		)
+	}
+
 	return (
 		<>
-			<button onClick={props.onCloseCart}>
+			<button
+				onClick={isLocationCart ? props.onCloseCart : undefined}
+				className="cursor-pointer"
+			>
 				cart{count > 0 && <span>({count})</span>}
 			</button>
-			{user && <button>saved</button>}
-			{user && <button>profile</button>}
-			{user && <button onClick={logout}>logout</button>}
-			{!user && (
-				<button>
-					<Link to={'/login'}>login</Link>
-				</button>
-			)}
+			{actions}
 		</>
 	)
 }
