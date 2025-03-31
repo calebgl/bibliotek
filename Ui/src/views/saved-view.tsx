1acb36c9c264eb267afe3be59d6123c2f3fa6f44
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-31 14:20:03 -0600

    fix(ui): disable buttons while list is loading

--- Ui/src/views/saved-view.tsx
+++ Ui/src/views/saved-view.tsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router'
 
+import { Button } from '../components/button'
 import { ButtonAddToCart } from '../components/button-add-to-cart'
 import { ButtonSaveBook } from '../components/button-save-book'
 import { useSavedBooks } from '../hooks/use-api'
 import { assert } from '../lib/assert'
 import { formatCurrency } from '../lib/utils'
-import { Button } from '../components/button'
 
 export function SavedView() {
 	return (
@@ -32,8 +32,14 @@ function List() {
 						<div className="h-5 w-48 animate-pulse bg-gray-100" />
 						<div className="h-6 w-24 animate-pulse bg-gray-200" />
 						<div className="mt-auto flex gap-4">
-							<Button className="h-12 min-w-32 animate-pulse bg-gray-200" />
-							<Button className="h-12 min-w-16 animate-pulse bg-gray-200" />
+							<Button
+								className="h-12 min-w-32 animate-pulse bg-gray-200"
+								disabled
+							/>
+							<Button
+								className="h-12 min-w-16 animate-pulse bg-gray-200"
+								disabled
+							/>
 						</div>
 					</div>
 				</div>
@@ -43,30 +49,31 @@ function List() {
 
 	assert(books)
 
-	return books.map((book) => (
-		<div key={'favorite-' + book.id} className="flex gap-8">
-			<Link to={'/books/' + book.id} className="aspect-2/3 w-40 max-w-40">
-				<img src={book.coverUrl ?? undefined} alt={book.title} />
-			</Link>
-			<div className="flex grow flex-col">
-				<Link
-					to={'/books/' + book.id}
-					className="text-2xl font-semibold"
-				>
-					{book.title}
+	return books.map((book) => {
+		const to = '/books/' + book.id
+		const coverUrl = book.coverUrl ?? undefined
+		return (
+			<div key={'favorite-' + book.id} className="flex gap-8">
+				<Link to={to} className="aspect-2/3 w-40 max-w-40">
+					<img src={coverUrl} alt={book.title} />
 				</Link>
-				<div className="flex items-center gap-1">
-					<span>★★★★☆</span>
-					<span className="text-sm">{book.averageRating}</span>
-					<span className="text-xs">({book.totalReviews})</span>
-				</div>
-				<div className="text-base">{book.author}</div>
-				<div className="text-xl">{formatCurrency(book.price)}</div>
-				<div className="mt-auto flex gap-4">
-					<ButtonAddToCart book={book} />
-					<ButtonSaveBook bookId={book.id} />
+				<div className="flex grow flex-col">
+					<Link to={to} className="text-2xl font-semibold">
+						{book.title}
+					</Link>
+					<div className="flex items-center gap-1">
+						<span>★★★★☆</span>
+						<span className="text-sm">{book.averageRating}</span>
+						<span className="text-xs">({book.totalReviews})</span>
+					</div>
+					<div className="text-base">{book.author}</div>
+					<div className="text-xl">{formatCurrency(book.price)}</div>
+					<div className="mt-auto flex gap-4">
+						<ButtonAddToCart book={book} />
+						<ButtonSaveBook bookId={book.id} />
+					</div>
 				</div>
 			</div>
-		</div>
-	))
+		)
+	})
 }
