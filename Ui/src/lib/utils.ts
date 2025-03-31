f28da316884cbd1fedbbf8ab665e3358944a50be
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-31 10:22:17 -0600

    feat: utilty function to merge tailwind classes based on conditions and order

--- Ui/src/lib/utils.ts
+++ Ui/src/lib/utils.ts
@@ -1,3 +1,7 @@
+import type { ClassValue } from 'clsx'
+import { clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 export function sleep(ms: number = 500): Promise<void> {
 	return new Promise((r) => setTimeout(r, ms))
 }
@@ -12,3 +16,7 @@ export const formatDecimal = new Intl.NumberFormat('es-mx', {
 	minimumFractionDigits: 2,
 	maximumFractionDigits: 2,
 }).format
+
+export function cn(...inputs: ClassValue[]): string {
+	return twMerge(clsx(inputs))
+}
