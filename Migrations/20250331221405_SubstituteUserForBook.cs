71f733456a4dd708ad99214fa96ff37eb7a71eda
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-31 16:19:08 -0600

    fix(api): substitude user for book

---
+++ Migrations/20250331221405_SubstituteUserForBook.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bibliotek.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubstituteUserForBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartBooks_AspNetUsers_UserId",
+                table: "CartBooks");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "CartBooks",
+                newName: "BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartBooks_UserId",
+                table: "CartBooks",
+                newName: "IX_CartBooks_BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartBooks_books_BookId",
+                table: "CartBooks",
+                column: "BookId",
+                principalTable: "books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartBooks_books_BookId",
+                table: "CartBooks");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "CartBooks",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartBooks_BookId",
+                table: "CartBooks",
+                newName: "IX_CartBooks_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartBooks_AspNetUsers_UserId",
+                table: "CartBooks",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Models/CartBook.cs
+++ Models/CartBook.cs
@@ -11,12 +11,12 @@ public class CartBook : ITimestamps
 
     public uint Id { get; set; }
     public uint CartId { get; set; }
-    public uint UserId { get; set; }
+    public uint BookId { get; set; }
     public uint Quantity { get; set; }
 
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 
     public required Cart Cart { get; set; }
-    public required User User { get; set; }
+    public required Book Book { get; set; }
 }
