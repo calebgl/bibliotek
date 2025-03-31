7ba210992d08b66147366c0a2c7d02331d8005e7
calebgl <57515964+calebgl@users.noreply.github.com>
2025-03-31 14:59:31 -0600

    wip(api): add cart logic "contract"

---
+++ Controllers/CartController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/cart")]
+class CartController(BibliotekContext context) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult ListCartBooks()
+    {
+        Utils.ValidateCurrentUser(User, context);
+        return Ok();
+    }
+
+    [HttpPost("books")]
+    public IActionResult AddBookToCart()
+    {
+        return Ok();
+    }
+
+    [HttpPut("books/{bookId}")]
+    public IActionResult UpdateCartBook([FromRoute] uint bookId)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("books")]
+    public IActionResult RemoveBookFromCart([FromRoute] uint bookId)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("books/{bookId}")]
+    public IActionResult ClearCart([FromRoute] uint bookId)
+    {
+        return Ok();
+    }
+
+    [HttpPost("checkout")]
+    public IActionResult CheckoutCart()
+    {
+        return Ok();
+    }
+}
