eceb12fc471b3695be3e4d3aa8cefc2d1218e801
calebgl <57515964+calebgl@users.noreply.github.com>
2025-04-01 11:20:16 -0600

    fix(api): sign out user with sign in manager if session is invalid

--- ValidateSession.cs
+++ ValidateSession.cs
@@ -1,38 +1,45 @@
 using System.Diagnostics;
 using System.Security.Claims;
+using Bibliotek.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-public class ValidateSession(BibliotekContext dbContext) : IAsyncActionFilter
+public class ValidateSession(BibliotekContext dbContext, SignInManager<User> signInManager)
+    : IAsyncActionFilter
 {
     public async Task OnActionExecutionAsync(
         ActionExecutingContext context,
         ActionExecutionDelegate next
     )
     {
-        var httpContext = context.HttpContext;
-        var contextClaim = context.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
         try
         {
+            var contextClaim = context.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
             Debug.Assert(contextClaim is not null);
 
             var userId = contextClaim.Value;
             Debug.Assert(!string.IsNullOrWhiteSpace(userId));
 
             var user = dbContext.Users.Where(u => u.Id == uint.Parse(userId)).FirstOrDefault();
-            Debug.Assert(user is not null);
+            if (user is null)
+            {
+                throw new InvalidOperationException("User not found?");
+            }
+
             Debug.Assert(!string.IsNullOrWhiteSpace(user.Email));
             Debug.Assert(!string.IsNullOrWhiteSpace(user.UserName));
 
-            httpContext.Items["CurrentUser"] = user;
+            context.HttpContext.Items["CurrentUser"] = user;
             await next();
         }
         catch (Exception ex)
         {
-            httpContext.Session.Clear();
+            await signInManager.SignOutAsync();
+
             Console.WriteLine(ex);
 
-            context.Result = new UnauthorizedObjectResult("Invalid session");
+            context.Result = new UnauthorizedObjectResult("Invalid user session");
         }
     }
 }
