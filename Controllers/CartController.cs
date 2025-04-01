8ad623d5b605516c010c587be6a05b0ac47b651f
calebgl <57515964+calebgl@users.noreply.github.com>
2025-04-01 14:01:05 -0600

    feat(api): add endpoints to add and list books from cart

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,4 +1,9 @@
+using System.Diagnostics;
+using Bibliotek.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace Bibliotek.Controllers;
 
 [ApiController]
@@ -6,15 +11,77 @@ namespace Bibliotek.Controllers;
 public class CartController(BibliotekContext context) : ControllerBase
 {
     [HttpGet]
+    [Authorize]
+    [ValidateSession]
     public IActionResult ListCartBooks()
     {
-        return Ok();
+        var user = HttpContext.GetCurrentUser();
+        var books = context
+            .Carts.Where(c => c.UserId == user.Id)
+            .SelectMany(c => c.CartBooks)
+            .Include(cb => cb.Book)
+            .Select(cb => new
+            {
+                cb.Book.Id,
+                cb.Book.Title,
+                cb.Book.Author,
+                cb.Book.Subtitle,
+                cb.Book.CoverUrl,
+                cb.Book.Price,
+                cb.Quantity,
+                AddedAt = cb.CreatedAt,
+            })
+            .ToList();
+
+        return Ok(books);
     }
 
     [HttpPost("books")]
-    public IActionResult AddBookToCart()
+    [Authorize]
+    [ValidateSession]
+    public IActionResult AddBookToCart(AddCartBookRequest request)
     {
-        return Ok();
+        var user = HttpContext.GetCurrentUser();
+        var cart = context.Carts.FirstOrDefault(c => c.UserId == user.Id);
+        Debug.Assert(cart is not null);
+
+        var book = context.Books.FirstOrDefault(b => b.Id == request.BookId);
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        var quantity = request.quantity ?? 1;
+        var cartBook = context.CartBooks.FirstOrDefault(cb =>
+            cb.CartId == cart.Id && cb.BookId == book.Id
+        );
+        if (cartBook is not null)
+        {
+            cartBook.Quantity += quantity;
+        }
+        else
+        {
+            cartBook = new CartBook
+            {
+                BookId = book.Id,
+                CartId = cart.Id,
+                Cart = cart,
+                Book = book,
+                Quantity = quantity,
+            };
+            context.CartBooks.Add(cartBook);
+        }
+
+        context.SaveChanges();
+
+        return Ok(
+            new
+            {
+                BookId = book.Id,
+                quantity = cartBook.Quantity,
+                AddedAt = cartBook.CreatedAt,
+            }
+        );
     }
 
     [HttpPut("books/{bookId}")]
@@ -41,3 +108,7 @@ public class CartController(BibliotekContext context) : ControllerBase
         return Ok();
     }
 }
+
+public record AddCartBookRequest(uint BookId, uint? quantity);
+
+public record UpdateCartBookRequest();
