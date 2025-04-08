262afc17592b855727d40184700838b08534806e
calebgl <57515964+calebgl@users.noreply.github.com>
2025-04-08 12:37:22 -0600

    feat(api): add property to book list if user is authenticated

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -28,8 +28,10 @@ public class BookController(BibliotekContext context) : ControllerBase
     }
 
     [HttpGet("{bookId}")]
+    [ValidateSession(throwOnValidationFailure: false)]
     public IActionResult ViewBook(uint bookId)
     {
+        var user = HttpContext.GetCurrentPosibleUser();
         var book = context
             .Books.Include(b => b.BookStats)
             .Select(b => new
@@ -58,6 +60,28 @@ public class BookController(BibliotekContext context) : ControllerBase
             return NotFound();
         }
 
-        return Ok(book);
+        if (user is null)
+        {
+            return Ok(book);
+        }
+
+        var obj = new Object();
+        var isSaved = context.SavedBooks.Any(sb => sb.BookId == bookId && sb.UserId == user.Id);
+
+        return Ok(
+            new
+            {
+                book.Id,
+                book.Title,
+                book.Author,
+                book.CoverUrl,
+                book.Description,
+                book.Price,
+                book.TotalReviews,
+                book.AverageRating,
+                book.Stars,
+                isSaved,
+            }
+        );
     }
 }
